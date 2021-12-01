35bd618cf25cfeead526abfeb61d1065d744c395
Zou Guangxian <zouguangxian@163.com>
2021-12-01 17:50:07 +0800

    fix: interrupted when font config exists.

--- setup-for-build.sh
+++ setup-for-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-FONT_CONFIG=`kpsewhich -var-value TEXMFSYSVAR`/fonts/conf/texlive-fontconfig.conf
+TEXLIVE_FONT_CONFIG=`kpsewhich -var-value TEXMFSYSVAR`/fonts/conf/texlive-fontconfig.conf
+FONT_CONFIG=/etc/fonts/conf.d/09-texlive.conf
+
 apt-get -y install --no-install-recommends \
   python3 python-is-python3 python3-pip \
   fonts-noto fonts-noto-mono fonts-noto-extra fonts-noto-cjk fonts-noto-cjk-extra \
-&& ( [[ ! -f "$FONT_CONFIG" ]] && ln -s $FONT_CONFIG /etc/fonts/conf.d/09-texlive.conf ) \
+&& ( [[ ! -f "$FONT_CONFIG" ]] && ln -s $TEXLIVE_FONT_CONFIG $FONT_CONFIG || true ) \
 && fc-cache -fsv
