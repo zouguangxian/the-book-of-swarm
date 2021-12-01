dc63368c8f3fab1059a1e3aea316ff121843a6c5
Zou Guangxian <zouguangxian@163.com>
2021-12-01 12:53:43 +0800

    build: setup environment and list required packages.

---
+++ setup-for-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt-get -y install --no-install-recommends \
+  python3 python-is-python3 python3-pip \
+  fonts-noto fonts-noto-extra fonts-noto-cjk fonts-noto-cjk-extra \
+&& ln -s `kpsewhich -var-value TEXMFSYSVAR`/fonts/conf/texlive-fontconfig.conf /etc/fonts/conf.d/09-texlive.conf \
+&& fc-cache -fsv
